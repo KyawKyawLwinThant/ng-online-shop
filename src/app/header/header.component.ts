f1adf88445821c66b4b54f9d84be1ba6d16a756b
Kyaw Kyaw Lwin <kyawkyawlwinthant@gmail.com>
2020-01-28 16:10:51 +0630

    cart 2

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,31 +1,21 @@
 import { CartService } from './../cart.service';
-import { Component, OnInit, OnChanges, AfterContentInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit,OnChanges,AfterContentInit {
+export class HeaderComponent implements OnInit{
   
-  ngAfterContentInit(): void {
-    this.cartSize=this.cart.cartSize();
-  }
-  //Called after ngOnInit when the component's or directive's content has been initialized.
-  //Add 'implements AfterContentInit' to the class.
   
-
-
-  ngOnChanges(changes: import("@angular/core").SimpleChanges): void {
-    this.cartSize=this.cart.cartSize();
-  }
   
-  cartSize:number=0;
-
-  constructor(private cart:CartService) { }
+  constructor() { }
 
   ngOnInit() {
-    this.cartSize=this.cart.cartSize();
+    
   }
 
 }
